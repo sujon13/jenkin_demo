36c7e6b2cf8da5eed59ea406d6e2d31a24451b32
Arifur Rahman Sujon <arifurrahmansujon27@gmail.com>
2023-08-20 20:42:48 +0600

    Added log in test endpoint

--- src/main/java/com/example/jenkin_demo/controller/TestController.java
+++ src/main/java/com/example/jenkin_demo/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.example.jenkin_demo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @Slf4j
@@ -12,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
-    public ResponseEntity<String> test() {
+    public ResponseEntity<String> test(HttpServletRequest request) {
+        log.info("Api Request Param: " + request.getRequestURI());
+        log.info("Time: " + new Date());
         return ResponseEntity.ok("Hello Man. I am running on port 9007");
     }
 }
